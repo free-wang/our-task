044d7a15accc5f51069ac5ce46064ae19fb4bb99
water76016 <dxylovelq@163.com>
2020-05-10 21:24:53 +0800

    添加bootstrap的前端代码

---
+++ src/main/java/com/example/demo/controller/CommonController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class CommonController {
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+}
