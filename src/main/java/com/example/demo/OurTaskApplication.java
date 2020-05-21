e1d890f9e76233bcc017b65df8fc32b705e851b9
water76016 <dxylovelq@163.com>
2020-05-21 22:25:21 +0800

    简单对网页进行维护

--- src/main/java/com/example/demo/OurTaskApplication.java
+++ src/main/java/com/example/demo/OurTaskApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
 @SpringBootApplication
 // 启动类必须加入@ServletComponentScan注解，否则无法扫描到servlet
 @ServletComponentScan
