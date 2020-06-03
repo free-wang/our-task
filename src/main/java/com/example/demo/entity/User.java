db2e12d79c5d40143bba6523d5b6c03a5c089f5f
water76016 <dxylovelq@163.com>
2020-06-03 23:18:45 +0800

    一个大的更新开始了

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String accountNumber;
+    private String password;
+    private Date createTime;
+    private Date updateTime;
+    private String telephone;
+    private String email;
+    private String headPortrait;
+    private Integer freeze;
+}
