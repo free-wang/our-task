e63cee52cf2fc18d37c9a7d9921af7fa7e7b29c3
water76016 <dxylovelq@163.com>
2020-09-20 22:09:46 +0800

    根据阿里开发手册，修改代码里面不规范的地方

--- java/src/main/java/com/water76016/ourtask/dto/TaskParam.java
+++ java/src/main/java/com/water76016/ourtask/dto/TaskParam.java
@@ -22,10 +22,16 @@ public class TaskParam {
      * 分类id
      */
     Integer categoryId;
-    //清单名称
+    /**
+     * 清单名称
+     */
     String name;
-    //所属标签列表
+    /**
+     * 所属标签列表
+     */
     List<Integer> labelList;
-    //清单描述
+    /**
+     * 清单描述
+     */
     String description;
 }
