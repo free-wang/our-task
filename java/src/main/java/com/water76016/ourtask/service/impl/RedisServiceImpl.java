c5e61bc68979e28c777fa6b28a39c314bc538398
water76016 <dxylovelq@163.com>
2020-09-26 23:47:19 +0800

    删除已经不用的注释代码

--- java/src/main/java/com/water76016/ourtask/service/impl/RedisServiceImpl.java
+++ java/src/main/java/com/water76016/ourtask/service/impl/RedisServiceImpl.java
@@ -42,22 +42,6 @@ public class RedisServiceImpl implements RedisService {
 
 
 
-
-//    /**
-//     * 修改Redis序列化方式，解决Redis可视化工具乱码问题
-//     * @return
-//     */
-//    @Bean
-//    public RedisTemplate redisTemplateInit() {
-//        //设置序列化Key的实例化对象
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        //设置序列化Value的实例化对象
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        return redisTemplate;
-//    }
-
-
-
     @Override
     public void set(String key, Object value, long time) {
         redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
