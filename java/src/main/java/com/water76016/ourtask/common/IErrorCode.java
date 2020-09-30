b44a3f7ca7f46437deebec189c1b2972ce6d530f
water76016 <dxylovelq@163.com>
2020-09-30 14:15:15 +0800

    对SpringSecurity的配置进行修改和维护

--- java/src/main/java/com/water76016/ourtask/common/IErrorCode.java
+++ java/src/main/java/com/water76016/ourtask/common/IErrorCode.java
@@ -5,7 +5,15 @@ package com.water76016.ourtask.common;
  * Created by macro on 2019/4/19.
  */
 public interface IErrorCode {
+    /**
+     * 得到状态码
+     * @return:状态码
+     */
     long getCode();
 
+    /**
+     * 获取返回消息
+     * @return:message返回消息
+     */
     String getMessage();
 }
