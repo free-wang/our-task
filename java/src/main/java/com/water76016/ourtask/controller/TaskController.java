302abe955e7a5413a902892ef70d7be49c6007ae
water76016 <dxylovelq@163.com>
2020-10-14 20:33:25 +0800

    对项目进行修改和维护

--- java/src/main/java/com/water76016/ourtask/controller/TaskController.java
+++ java/src/main/java/com/water76016/ourtask/controller/TaskController.java
@@ -94,7 +94,6 @@ public class TaskController {
         }
         return RestResult.success("得到当前用户所有未完成清单成功", taskParamList);
     }
-
     @ApiOperation("查询当前用户当前分类的所有未完成清单")
     @GetMapping("getAllList/{userId}/{categoryId}")
     public RestResult getTaskList(@PathVariable("userId") Integer userId, @PathVariable("categoryId") Integer categoryId){
@@ -141,7 +140,6 @@ public class TaskController {
         }
         return RestResult.success("得到当前分页清单成功", taskPage);
     }
-
     @ApiOperation("获取最近一周的清单完成情况")
     @GetMapping("/countTaskForDay/{userId}")
     public RestResult countTaskForDay(@PathVariable("userId") Integer userId){
