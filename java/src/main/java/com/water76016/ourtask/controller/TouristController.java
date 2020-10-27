4093fdb6851d536a564bd14775d773501db64f9e
water76016 <dxylovelq@163.com>
2020-10-27 18:54:37 +0800

    更新注释代码

--- java/src/main/java/com/water76016/ourtask/controller/TouristController.java
+++ java/src/main/java/com/water76016/ourtask/controller/TouristController.java
@@ -31,7 +31,6 @@ public class TouristController {
     public RestResult register(@RequestBody Tourist tourist){
         return touristService.register(tourist);
     }
-
     @ApiOperation("游客进行登录操作")
     @PostMapping({"/login"})
     public RestResult login(@RequestBody Tourist tourist, HttpServletResponse response) {
