7dfdc5eb7f37e78c650bd0473ff000a720965017
water76016 <dxylovelq@163.com>
2020-11-28 22:30:03 +0800

    删除其他测试类的登录测试，为了测试方便，以后测试可以把springsecurity的拦截暂时关闭

--- java/src/test/java/com/water76016/ourtask/controller/FeedbackControllerTest.java
+++ java/src/test/java/com/water76016/ourtask/controller/FeedbackControllerTest.java
@@ -8,13 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,19 +24,6 @@ class FeedbackControllerTest {
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
-
-    String login() throws Exception {
-        User user = new User("user", "123456");
-        String content = JSONObject.toJSONString(user);
-        String result = mockMvc.perform(post("/login")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(content))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        RestResult restResult = JSONObject.parseObject(result, RestResult.class);
-        assert restResult.get("code").equals(200);
-        return null;
     }
 
     @Test
